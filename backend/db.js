b3a407f5105ccaefc7fac46306dafa43879d4846
Gonza <gnzx89@gmail.com>
2025-04-23 15:13:47 -0300

    practicandoinsert

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,14 @@ const db = mysql.createConnection({
     database:'sql10774543'
 })
 
+db.connect((err) => {
+    if (err) {
+      console.error('❌ Error al conectar a la base de datos:', err);
+    } else {
+      console.log('✅ Conectado a la base de datos MySQL');
+    }
+  });
+
 export default db;
 
 
--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,31 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json()); // Middleware para procesar JSON en el body de las peticiones
 
-app.get('/api/profesionales', (req,res) => {
+app.get('/api/profesionales', (req, res) => {
   db.query('SELECT * FROM profesionales', (err, results) => {
-    if(err){
+    if (err) {
       console.error('Error al obtener los resultados')
       res.status(500).send('Error al obtener los resultados')
     }
 
     res.json(results)
+  })
+})
+
+app.post('/api/profesional', (req, res) => {
+  const { nombre, apellido, especialidad, matricula } = req.body
+
+  const sql = 'INSERT INTO profesional (nombre,apellido,especialidad,matricula) VALUES (?,?,?,?)';
+
+  db.query(sql, [nombre, apellido, especialidad, matricula], (err, results) => {
+    if (err) {
+      console.error('Erorr al insertar profesional');
+      res.status(500).send('Error al insertar profesional');
+    }
+    res.status(201).json({
+      mensaje: 'Profesional creado',
+      id: results.insertId
+    });
   })
 })
 
